a49821c57fe126f8c2841df076472372be32ceb6
AndreyFilchenkov <AndreyFilchenkov@users.noreply.github.com>
2018-02-09 11:01:31 +0200

    Project files

---
+++ src/Sitecore.Support.36156/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.36156")]
+[assembly: AssemblyProduct("Sitecore.Support.36156")]
+[assembly: ComVisible(false)]
